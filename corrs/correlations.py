6a6727202a99f1a216bc77d65d984cc44dbd223e
fanourakis <fanourakis@ics.forth.gr>
2022-12-12 16:37:44 +0200

    correlations

--- corrs/correlations.py
+++ corrs/correlations.py
@@ -3,56 +3,68 @@ import statistics as st
 from scipy.stats.stats import spearmanr
 from scipy.stats.stats import pearsonr
 
-RREA_df = pd.read_excel("RREA.xlsx", convert_float=True, engine='openpyxl')
-metrics_df = pd.read_excel("metrics.xlsx", convert_float=True, engine='openpyxl')
+RREA_df = pd.read_excel("MultiKE.xlsx", convert_float=True, engine='openpyxl')
+metrics_df = pd.read_excel("metrics_kg2.xlsx", convert_float=True, engine='openpyxl')
 
 hits_1 = []
 hits_5 = []
 hits_10 = []
+mrr = []
 for index, row in RREA_df.iterrows():
-    if index == 0 or index == 3 or index == 6:
+    if index == 0 or index == 4 or index == 8:
         for r in list(row):
             hits_1.append(r)
-    elif index == 1 or index == 4 or index == 7:
+    elif index == 1 or index == 5 or index == 9:
          for r in list(row):
             hits_5.append(r)
-    elif index == 2 or index == 5 or index == 8:
+    elif index == 2 or index == 6 or index == 10:
          for r in list(row):
             hits_10.append(r)
+    elif index == 3 or index == 7 or index == 11:
+         for r in list(row):
+            mrr.append(r)
+
 wcc = []
-b_index = []
-ccc = []
+deg = []
+maxCS = []
 for index, row in metrics_df.iterrows():
     if index == 0 or index == 3 or index == 6:
          for r in list(row):
             wcc.append(r)
     elif index == 1 or index == 4 or index == 7:
          for r in list(row):
-            b_index.append(r)
+            maxCS.append(r)
     elif index == 2 or index == 5 or index == 8:
         for r in list(row):
-            ccc.append(r)
+            deg.append(r)
+
 h_1_wcc = spearmanr(hits_1, wcc)
-h_1_b_index = spearmanr(hits_1, b_index)
-h_1_ccc = spearmanr(hits_1, ccc)
+h_1_deg = spearmanr(hits_1, deg)
+h_1_maxCS = spearmanr(hits_1, maxCS)
 h_5_wcc = spearmanr(hits_5, wcc)
-h_5_b_index = spearmanr(hits_5, b_index)
-h_5_ccc = spearmanr(hits_5, ccc)
+h_5_deg = spearmanr(hits_5, deg)
+h_5_maxCS = spearmanr(hits_5, maxCS)
 h_10_wcc = spearmanr(hits_10, wcc)
-h_10_b_index = spearmanr(hits_10, b_index)
-h_10_ccc = spearmanr(hits_10, ccc)
+h_10_deg = spearmanr(hits_10, deg)
+h_10_maxCS = spearmanr(hits_10, maxCS)
+mrr_wcc = spearmanr(mrr, wcc)
+mrr_deg = spearmanr(mrr, deg)
+mrr_maxCS = spearmanr(mrr, maxCS)
 print(h_1_wcc)
-print(h_1_b_index)
-print(h_1_ccc)
+print(h_1_deg)
+print(h_1_maxCS)
 print(h_5_wcc)
-print(h_5_b_index)
-print(h_5_ccc)
+print(h_5_deg)
+print(h_5_maxCS)
 print(h_10_wcc)
-print(h_10_b_index)
-print(h_10_ccc)
+print(h_10_deg)
+print(h_10_maxCS)
+print(mrr_wcc)
+print(mrr_deg)
+print(mrr_maxCS)
 corr_df = pd.DataFrame({
-    "wcc": [h_1_wcc[0], h_5_wcc[0], h_10_wcc[0]],
-    "b_index": [h_1_b_index[0], h_5_b_index[0], h_10_b_index[0]],
-    "CCC": [h_1_ccc[0], h_5_ccc[0], h_10_ccc[0]]
-}).rename(index={0: "Hits@1", 1: "Hits@5", 2: "Hits@10"}).T
+    "wcc": [h_1_wcc[0], h_5_wcc[0], h_10_wcc[0], mrr_wcc[0]],
+    "deg": [h_1_deg[0], h_5_deg[0], h_10_deg[0], mrr_deg[0]],
+    "maxCS": [h_1_maxCS[0], h_5_maxCS[0], h_10_maxCS[0], mrr_maxCS[0]]
+}).rename(index={0: "Hits@1", 1: "Hits@5", 2: "Hits@10", 3:"MRR"})
 print(corr_df)
